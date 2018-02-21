322f6697dd4cc28bc6f5f998bd926943b487eb68
Trevor Johnson <johnsontrevor55@gmail.com>
2018-02-21 00:25:51 -0700

    styled

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   var selectOponent = false;
   var canAttack = false;
   var vsPlayerH2Span;
-  var versesH2 = $(`<h2 class="text-center mb-4"></h2>`);
+  var versesH2 = $(`<h2 class="text-center mb-4 alert alert-warning w-50 mx-auto"></h2>`);
   var attackBtn = $(`<div class="mb-3 mb-md-0 text-center"><button type="button" class="btn btn-success" id="attack-btn">Attack</button></div>`);
   var healthBar = $(`<div class="progress"><div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div></div>`);
 
@@ -20,10 +20,14 @@ $(document).ready(function() {
   }
 
   //sets character object to their html element
-  $('#luke').data(new Character(225, 225, 20, 20, 15));
+  $('#luke').data(new Character(225, 225, 20, 20, 18));
   $('#kylo').data(new Character(130, 130, 10, 10, 7));
-  $('#rey').data(new Character(120, 120, 7, 7, 5));
-  $('#snoke').data(new Character(165, 165, 15, 15, 10));
+  $('#rey').data(new Character(125, 125, 7, 7, 5));
+  $('#snoke').data(new Character(165, 165, 15, 15, 9));
+
+  // $('.close').on('click', function() {
+  //   location.reload();
+  // });
 
   //attaches click event to attack-btn. Attached to .verses div, and then specifies a selector because the btn was dynamically generated
   $('.verses').on('click', '#attack-btn', function() {
@@ -69,27 +73,31 @@ $(document).ready(function() {
     if (activeOpponent.health === 0) {
       canAttack = false;
 
-      setTimeout(function() {
-        $('#' + activeOpponent.name).parent().hide();
-        $(versesH2).html('Choose Next Opponent');
-        selectOponent = true;
-
-        //restarts game if it is over
-        if ($('.all-characters .d-flex').children().length === 0) {
+      //restarts game if it is over
+      if ($('.all-characters .d-flex').children().length === 0) {
+        $(versesH2).removeClass('alert-warning').addClass('alert-success').html(`You Won!`);
+        setTimeout(function() {
           location.reload();
+        }, 2000);
+      } else {
+          $(versesH2).html(`You defeated ${activeOpponent.name}!`);
+          setTimeout(function() {
+            $('#' + activeOpponent.name).parent().hide();
+            $(versesH2).html('Choose Next Opponent');
+            selectOponent = true;
+          }, 1500);      
         }
-      }, 1500);
 
       $(attackBtn).toggle();
-      $(versesH2).html('You Win!');
     }
 
     if (activeCharacter.health === 0) {
       canAttack = false;
-      $(versesH2).html('Game Over');
+      $(versesH2).removeClass('alert-warning').addClass('alert-danger').html(`Game Over`);
+      $('.alert-danger').removeClass('d-none')
       setTimeout(function() {
         location.reload();
-      }, 1500);
+      }, 2000);
     }
   });
 
@@ -112,7 +120,7 @@ $(document).ready(function() {
       var cardBody = $(this).find('.card-body');
       $(healthBar).appendTo(cardBody);
 
-      //selects players data-name and adds it to h2 tag in verses area
+      //selects players name and adds it to h2 tag in verses area
       vsPlayerH2Span = $(`<span>${$(this).find('.card-title').text()} VS. </span>`);
       $(vsPlayerH2Span).appendTo(versesH2);
       $(versesH2).prependTo('.fight-area');
@@ -142,7 +150,7 @@ $(document).ready(function() {
       var cardBody = $(this).find('.card-body');
       $(healthBar).clone().appendTo(cardBody);
   
-      //selects current oppoents data-name and adds it to h2 tag in verses area
+      //selects current oppoents name and adds it to h2 tag in verses area
       var vsEnemyH2Span = $(`<span>${$(this).find('.card-title').text()}</span>`);
       $(versesH2).empty();
       $(vsPlayerH2Span).appendTo(versesH2);
