571f82bee67ac1e3bf9842b39288c94c0517fbda
Trevor Johnson <johnsontrevor55@gmail.com>
2018-02-23 19:13:06 -0700

    switched verses h2 so it starts off in html instead of js

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   var selectOponent = false;            //if true, you can select a new opponent.
   var canAttack = false;                //if false, you cannot attack and attack button does not show up
   var vsPlayerH2Span;                   //h2 changes to display current character and opponent, and only shows up when character is selected.
-  var versesH2 = $(`<h2 class="text-center mb-4 alert alert-warning w-50 mx-auto"></h2>`);
+  // var versesH2 = $(`<h2 class="text-center mb-4 alert alert-warning w-50 mx-auto"></h2>`);
   var attackBtn = $(`<div class="mb-3 mb-md-0 text-center"><button type="button" class="btn btn-success mb-2" id="attack-btn">Attack</button><div id="damage-pts"></div></div>`);
   var healthBar = $(`<div class="progress"><div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div></div>`);
   var opponentHealthBar = $(`<div class="progress"><div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div></div>`);
@@ -59,7 +59,8 @@ $(document).ready(function() {
         //runs when character health is less than 0. displays an empty health bar, and alerts the game over modal.
         activeCharacter.health = 0;
         canAttack = false;
-        $(versesH2).remove();
+        // $(versesH2).remove();
+        // $('.fight-area h2').addClass('d-none');
         $('#' + activeCharacter.name).find('.card-body p').text(activeCharacter.health);
         $('#' + activeCharacter.name).find('.bg-danger').css('width', '0');
         $('.alert-danger').removeClass('d-none')
@@ -80,21 +81,23 @@ $(document).ready(function() {
         $('#' + activeOpponent.name).find('.card-body p').text(activeOpponent.health);
         $('#' + activeOpponent.name).find('.bg-danger').css('width', '0');
 
-        //If all opponents are defeated then the game is over
+        //If all opponents are defeated then the game is over. shows modal.
         if ($('.all-characters .d-flex').children().length === 0) {
-          $(versesH2).remove();
+          $('.fight-area h2').hide();
           $('#new-game-modal').modal('show');
 
         //allows selection of new opponent if one still exists.
         } else {
-          $(versesH2).html(`You defeated ${activeOpponent.name}!`);
+          $('.fight-area h2').html(`You defeated ${activeOpponent.name}!`);
 
           //Removes defeated opponent
           setTimeout(function() {
             $('#' + activeOpponent.name).parent().hide();
             $(attackBtn).toggle();
+
+            //removes display of damage
             $('#damage-pts').html('');
-            $(versesH2).html('Choose Next Opponent');
+            $('.fight-area h2').html('Choose Next Opponent');
             selectOponent = true;
           }, 1500);     
         }
@@ -109,8 +112,10 @@ $(document).ready(function() {
       activeCharacter = $(this).data();
       selectCharacter = false;
       selectOponent = true;
+
       //specifies your character
       $(this).addClass('selected-character');
+
       //hides tooltip when character is selected
       $('#' + activeCharacter.name).tooltip('dispose');
       $('.main-header').text('Choose Your Opponent');
@@ -123,9 +128,10 @@ $(document).ready(function() {
       $(healthBar).appendTo($(this).find('.card-body'));
 
       //selects players name and adds it to h2 tag in verses area
-      vsPlayerH2Span = $(`<span>${$(this).find('.card-title').text()} VS. </span>`);
-      $(vsPlayerH2Span).appendTo(versesH2);
-      $(versesH2).prependTo('.fight-area');
+      // vsPlayerH2Span = $(`<span>${$(this).find('.card-title').text()} VS. </span>`);
+      // $(vsPlayerH2Span).appendTo(versesH2);
+      // $(versesH2).prependTo('.fight-area');
+      $('.fight-area h2').html(`<span>${$(this).find('.card-title').text()} VS. </span>`).removeClass('d-none');
     }
 
 /////selects current oponent
@@ -158,11 +164,12 @@ $(document).ready(function() {
       $(opponentHealthBar).appendTo($(this).find('.card-body'));
       
       //selects current opponents name and adds it to h2 tag in verses area
-      var vsEnemyH2Span = $(`<span>${$(this).find('.card-title').text()}</span>`);
-      $(versesH2).empty();
-      $(vsPlayerH2Span).appendTo(versesH2);
-      $(vsEnemyH2Span).appendTo(versesH2);
-      $('.main-header').text('Remaining Opponents');
+      // var vsEnemyH2Span = $(`<span>${$(this).find('.card-title').text()}</span>`);
+      // $(versesH2).empty();
+      // $(vsPlayerH2Span).appendTo(versesH2);
+      // $(vsEnemyH2Span).appendTo(versesH2);
+      // $('.main-header').text('Remaining Opponents');
+      $('.fight-area h2').html(`<span>${activeCharacter.name} VS. ${$(this).find('.card-title').text()}</span>`);
     }
   });
 });
